be24c81161243618e41c1e6f51a1a4326a33e21b
André <andre_farao123@hotmail.com>
2022-12-08 01:03:31 -0300

    Added default layout with router dom

--- 02-ignite-timer/src/Router.tsx
+++ 02-ignite-timer/src/Router.tsx
@@ -1,11 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from './layouts/DefaultLayout';
+import { Home } from './pages/Home';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<Home />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<Home />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
---
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from './styles';
+import { Scroll, Timer } from 'phosphor-react';
+
+import logoIgnite from '../../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+};
